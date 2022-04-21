3b98b17b335860def1c8f1912f76a8325d1ebe9a
MadeYoga <vngla21@gmail.com>
2022-04-22 00:25:19 +0800

    Remove unused code & Add basic endpoints demo

---
+++ AuthEndpoints.Demo/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using AuthEndpoints.Demo.Models;
+using AuthEndpoints.Jwt.Controllers;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthEndpoints.Demo.Controllers;
+public class UserController : BasicEndpointsController<string, MyCustomIdentityUser>
+{
+    public UserController(UserManager<MyCustomIdentityUser> userRepository, IdentityErrorDescriber errorDescriber) : base(userRepository, errorDescriber)
+    {
+    }
+}
--- AuthEndpoints.Demo/Models/Responses/AuthenticatedUserResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AuthEndpoints.Demo.Models.Responses;
-
-public class AuthenticatedUserResponse
-{
-    public string? AccessToken { get; set; }
-    public string? RefreshToken { get; set; }
-}
