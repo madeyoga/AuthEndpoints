0add412bb12778ee075bb142a5382d368d6c8fcd
MadeYoga <vngla21@gmail.com>
2022-05-15 16:29:48 +0800

    Update demo project

--- AuthEndpoints.Demo/Program.cs
+++ AuthEndpoints.Demo/Program.cs
@@ -1,9 +1,13 @@
 using AuthEndpoints.Demo.Data;
 using AuthEndpoints.Demo.Models;
+using AuthEndpoints.Demo.Services;
 using AuthEndpoints.Extensions;
+using AuthEndpoints.Options;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -54,7 +58,40 @@ builder.Services.AddIdentityCore<MyCustomIdentityUser>(option =>
 	option.Password.RequiredLength = 0;
 }).AddEntityFrameworkStores<MyDbContext>();
 
-builder.Services.AddAuthEndpoints<string, MyCustomIdentityUser>();
+var accessTokenValidationParameters = new TokenValidationParameters()
+{
+	IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("9GHdZCAJ2XaXFuhOhIt21zxJCWk7obnzcHqDB4t7X0WcvrB8bzvkyEFlIMRXO4o-y3eQs8e4uDiFJcAhnFOiE6I45aJQi22DEy5epVLyQIVFYI-dbumj8ieK1sKMPySfN9S4eliQznJYL82XhtI_8U1EvEL2_C7PX4rTR0Xjf8k")),
+	ValidIssuer = "https://localhost:8000",
+	ValidAudience = "https://localhost:8000",
+	ValidateIssuerSigningKey = true,
+	ValidateIssuer = true,
+	ValidateAudience = true,
+	ClockSkew = TimeSpan.Zero,
+};
+
+var refreshTokenValidationParameters = new TokenValidationParameters()
+{
+	IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("8GHdZCAJ2XaXFuhOhIt21zxJCWk7obnzcHqDB4t7X0WcvrB8bzvkyEFlIMRXO4o-y3eQs8e4uDiFJcAhnFOiE6I45aJQi22DEy5epVLyQIVFYI-dbumj8ieK1sKMPySfN9S4eliQznJYL82XhtI_8U1EvEL2_C7PX4rTR0Xjf8k")),
+	ValidIssuer = "https://localhost:8000",
+	ValidAudience = "https://localhost:8000",
+	ValidateIssuerSigningKey = true,
+	ValidateIssuer = true,
+	ValidateAudience = true,
+	ClockSkew = TimeSpan.Zero,
+};
+
+builder.Services.AddAuthEndpoints<string, MyCustomIdentityUser>(new AuthEndpointsOptions()
+{
+	AccessTokenSecret = "9GHdZCAJ2XaXFuhOhIt21zxJCWk7obnzcHqDB4t7X0WcvrB8bzvkyEFlIMRXO4o-y3eQs8e4uDiFJcAhnFOiE6I45aJQi22DEy5epVLyQIVFYI-dbumj8ieK1sKMPySfN9S4eliQznJYL82XhtI_8U1EvEL2_C7PX4rTR0Xjf8k",	
+	RefreshTokenSecret = "8GHdZCAJ2XaXFuhOhIt21zxJCWk7obnzcHqDB4t7X0WcvrB8bzvkyEFlIMRXO4o-y3eQs8e4uDiFJcAhnFOiE6I45aJQi22DEy5epVLyQIVFYI-dbumj8ieK1sKMPySfN9S4eliQznJYL82XhtI_8U1EvEL2_C7PX4rTR0Xjf8k",
+	AccessTokenExpirationMinutes = 15,
+	RefreshTokenExpirationMinutes = 6000,
+	Audience = "https://localhost:8000",
+	Issuer = "https://localhost:8000",
+	AccessTokenValidationParameters = accessTokenValidationParameters,
+	RefreshTokenValidationParameters = refreshTokenValidationParameters
+}).AddJwtBearerAuthenticationScheme(accessTokenValidationParameters)
+	.AddClaimsProvider<MyClaimsProvider>();
 
 var app = builder.Build();
 
