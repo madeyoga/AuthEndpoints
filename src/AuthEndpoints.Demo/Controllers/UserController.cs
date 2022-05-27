01035ec1047f2e10baecb387b490b45be1884648
MadeYoga <vngla21@gmail.com>
2022-05-28 00:25:13 +0800

    Update demo project

--- src/AuthEndpoints.Demo/Controllers/UserController.cs
+++ src/AuthEndpoints.Demo/Controllers/UserController.cs
@@ -1,12 +1,13 @@
 ﻿using AuthEndpoints.Controllers;
 using AuthEndpoints.Demo.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
 
 namespace AuthEndpoints.Demo.Controllers;
 
 public class UserController : BaseEndpointsController<string, MyCustomIdentityUser>
 {
-    public UserController(UserManager<MyCustomIdentityUser> userRepository, IdentityErrorDescriber errorDescriber) : base(userRepository, errorDescriber)
+    public UserController(UserManager<MyCustomIdentityUser> userManager, IdentityErrorDescriber errorDescriber, IOptions<AuthEndpointsOptions> options) : base(userManager, errorDescriber, options)
     {
     }
 }
