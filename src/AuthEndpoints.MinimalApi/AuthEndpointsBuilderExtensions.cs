0ad0db0d201e7022c861a3226f68164ed59a9fc3
MadeYoga <vngla21@gmail.com>
2022-06-18 16:09:03 +0800

    Update minimal api extensions

--- src/AuthEndpoints.MinimalApi/AuthEndpointsBuilderExtensions.cs
+++ src/AuthEndpoints.MinimalApi/AuthEndpointsBuilderExtensions.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace AuthEndpoints.MinimalApi;
 
@@ -7,25 +7,25 @@ public static class AuthEndpointsBuilderExtensions
     public static AuthEndpointsBuilder AddBasicAuthenticationEndpoints(this AuthEndpointsBuilder builder)
     {
         var type = typeof(BasicAuthEndpointDefinition<,>).MakeGenericType(builder.UserKeyType, builder.UserType);
-        builder.Services.TryAddSingleton(type);
+        builder.Services.AddSingleton(typeof(IEndpointDefinition), type);
         return builder;
     }
 
     public static AuthEndpointsBuilder AddJwtEndpoints(this AuthEndpointsBuilder builder)
     {
         var type = typeof(JwtEndpointDefinition<,>).MakeGenericType(builder.UserKeyType, builder.UserType);
-        builder.Services.TryAddSingleton(type);
+        builder.Services.AddSingleton(typeof(IEndpointDefinition), type);
         return builder;
     }
 
     public static AuthEndpointsBuilder Add2FAEndpoints(this AuthEndpointsBuilder builder)
     {
         var type = typeof(TwoFactorEndpointDefinition<,>).MakeGenericType(builder.UserKeyType, builder.UserType);
-        builder.Services.TryAddSingleton(type);
+        builder.Services.AddSingleton(typeof(IEndpointDefinition), type);
         return builder;
     }
 
-    public static AuthEndpointsBuilder AddAllEndpointDefinitions( this AuthEndpointsBuilder builder)
+    public static AuthEndpointsBuilder AddAllEndpointDefinitions(this AuthEndpointsBuilder builder)
     {
         AddBasicAuthenticationEndpoints(builder);
         AddJwtEndpoints(builder);
--- src/AuthEndpoints.MinimalApi/WebApplicationExtensions.cs
+++ src/AuthEndpoints.MinimalApi/WebApplicationExtensions.cs
@@ -3,33 +3,12 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace AuthEndpoints.MinimalApi;
 
-internal static class WebApplicationExtensions
+public static class WebApplicationExtensions
 {
-    public static void MapBasicAuthenticationEndpoints(this WebApplication app)
-    {
-        var definitions = app.Services.GetServices<IEndpointDefinition>();
-        var definition = definitions.First(definition => definition.GetType().Name == "");
-        definition.MapEndpoints(app);
-    }
-
-    public static void MapJwtEndpoints(this WebApplication app)
-    {
-        var definitions = app.Services.GetServices<IEndpointDefinition>();
-        var definition = definitions.First(definition => definition.GetType().Name == "");
-        definition.MapEndpoints(app);
-    }
-
-    public static void Map2FAEndpoints(this WebApplication app)
-    {
-        var definitions = app.Services.GetServices<IEndpointDefinition>();
-        var definition = definitions.First(definition => definition.GetType().Name == "");
-        definition.MapEndpoints(app);
-    }
-
     public static void MapAuthEndpoints(this WebApplication app)
     {
         var definitions = app.Services.GetServices<IEndpointDefinition>();
-        foreach(var definition in definitions)
+        foreach (var definition in definitions)
         {
             definition.MapEndpoints(app);
         }
