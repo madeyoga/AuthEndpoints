3ee5af9de5c42c6e8edf485d14fe8c8f2064fd60
MadeYoga <vngla21@gmail.com>
2022-06-19 16:49:55 +0800

    Update demo

--- src/AuthEndpoints.Demo/Program.cs
+++ src/AuthEndpoints.Demo/Program.cs
@@ -89,11 +89,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
-
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.MapAuthEndpoints();
 
 app.Run();
