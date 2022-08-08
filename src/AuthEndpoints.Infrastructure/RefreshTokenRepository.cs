2df2732d4d5e02bfb3902bfeffc37914bb2c75aa
MadeYoga <vngla21@gmail.com>
2022-08-08 13:33:23 +0800

    feat: add refresh token repository

---
+++ src/AuthEndpoints.Infrastructure/RefreshTokenRepository.cs
@@ -0,0 +1,37 @@
+﻿using AuthEndpoints.Core.Models;
+using AuthEndpoints.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthEndpoints.Infrastructure;
+
+public class RefreshTokenRepository<TContext> : IRefreshTokenRepository where TContext : DbContext
+{
+    private readonly TContext _context;
+
+    public RefreshTokenRepository(TContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(RefreshToken refreshToken)
+    {
+        await _context.AddAsync(refreshToken);
+    }
+
+    public async Task<RefreshToken?> GetByIdAsync(int id)
+    {
+        RefreshToken? result = await _context.Set<RefreshToken>().FindAsync(id);
+        return result;
+    }
+
+    public async Task<RefreshToken?> GetByTokenAsync(string token)
+    {
+        RefreshToken? result = await _context.Set<RefreshToken>().Where((t) => t.Token == token).FirstOrDefaultAsync();
+        return result;
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+}
