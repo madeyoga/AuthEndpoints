6cd1aa6c74578ab97925c933d746925a4082a813
MadeYoga <vngla21@gmail.com>
2022-08-19 15:57:29 +0800

    feat: add token bearer defaults

---
+++ src/AuthEndpoints.TokenAuth.Core/TokenBearerDefaults.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthEndpoints.TokenAuth.Core;
+public static class TokenBearerDefaults
+{
+    /// <summary>
+    /// Default value for AuthenticationScheme property in the TokenBearerAuthenticationsOptions
+    /// </summary>
+    public const string AuthenticationScheme = "TokenBearer";
+}
