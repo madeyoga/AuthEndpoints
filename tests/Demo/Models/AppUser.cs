aa7cc3bf3f48d5e7c0d4d7e986af247e4f5f2110
MadeYoga <vngla21@gmail.com>
2026-07-30 15:52:21 +0800

    Remove Demo project

--- tests/Demo/Models/AppUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Demo.Models;
-
-public class AppUser : IdentityUser
-{
-    public bool IsSuperUser { get; set; } = false;
-    public string DisplayName { get; set; } = string.Empty;
-}
--- tests/Demo/Models/Blog.cs
+++
@@ -1,8 +0,0 @@
-namespace Demo;
-
-public class Blog
-{
-    public string Id { get; set; } = Guid.NewGuid().ToString();
-
-    public string Name { get; set; } = string.Empty;
-}
